c0ca94f49c41b32df58b3eee151750be2e8b5773
Kaung Myat Han <arkarhtethan@gmail.com>
2023-12-19 21:48:09 +0630

    update(): json derictive added

--- domain/customer.go
+++ domain/customer.go
@@ -3,12 +3,12 @@ package domain
 import "github.com/kaungmyathan22/golang-rest-microservice-banking-api/exception"
 
 type Customer struct {
-	Id          string `db:"customer_id"`
-	Name        string
-	City        string
-	Zipcode     string
-	DateOfBirth string `db:"date_of_birth"`
-	Status      string
+	Id          string `db:"customer_id" json:"customer_id"`
+	Name        string `json:"name"`
+	City        string `json:"city"`
+	Zipcode     string `json:"zipCode"`
+	DateOfBirth string `db:"date_of_birth" json:"date_of_birth"`
+	Status      string `json:"status"`
 }
 type CustomerRepository interface {
 	FindAll(string) ([]Customer, *exception.AppError)
